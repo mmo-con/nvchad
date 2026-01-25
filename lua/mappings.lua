812b2da9696c3cebe0b90b4caf94ec2a23b3fb98
Markus Moll <markus.moll@hacon.de>
2026-01-25 18:40:13 +0100

    Some initial mappings and plugins

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,34 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("i", "jk", "<ESC>")
+
+map('n', 'ö', 'l', { desc = 'Move cursor right' })
+map('n', 'l', 'k', { desc = 'Move cursor up' })
+map('n', 'k', 'j', { desc = 'Move cursor down' })
+map('n', 'j', 'h', { desc = 'Move cursor left' })
+
+map('v', 'ö', 'l', { desc = 'Move cursor right' })
+map('v', 'l', 'k', { desc = 'Move cursor up' })
+map('v', 'k', 'j', { desc = 'Move cursor down' })
+map('v', 'j', 'h', { desc = 'Move cursor left' })
+
+map('n', '<Space>ö', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map('n', '<Space>l', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+map('n', '<Space>k', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map('n', '<Space>j', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+
+map('n', '<C-Left>', ':vs<CR>', { desc = 'Move focus to the left window' })
+map('n', '<C-Right>', ':vs<CR>', { desc = 'Move focus to the left window' })
+map('n', '<C-Down>', ':sp<CR>', { desc = 'Move focus to the left window' })
+map('n', '<C-Up>', ':sp<CR>', { desc = 'Move focus to the left window' })
+
+map('i', 'jk', '<Esc>', { desc = 'Quickly escape from insert mode' })
+map('n', 'H', '^', { desc = 'Move to the very left border' })
+map('n', 'L', '$', { desc = 'Move to the very right border' })
+map('v', 'L', 'g_', { desc = 'Move to the very right border' })
+map('n', 'U', 'i<cr><esc>^mwgk:silent! s/\v +$//<cr>:noh<cr>`w', { desc = 'Split lines' })
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,12 @@ require "nvchad.options"
 
 -- add yours here!
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
