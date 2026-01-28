73a2b77edcb3589cc9fd8d45f33354fa0e27f315
Markus Moll <markus.moll@hacon.de>
2026-01-28 21:03:45 +0100

    Enable relative linenumbers by default

--- lua/options.lua
+++ lua/options.lua
@@ -9,6 +9,7 @@ vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.shell = 'zsh'
+vim.opt.relativenumber = true
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
