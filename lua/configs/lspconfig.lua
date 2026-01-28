198d6d392dc0ac3a00c9de378568dbbbc3f34940
Markus Moll <markus.moll@hacon.de>
2026-01-28 22:17:59 +0100

    Configure clangd LSP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,48 @@
-require("nvchad.configs.lspconfig").defaults()
+local nvchadconfig = require "nvchad.configs.lspconfig"
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
+local on_attach = nvchadconfig.on_attach
+local on_init = nvchadconfig.on_init
+local capabilities = nvchadconfig.capabilities
 
--- read :h vim.lsp.config for changing options of lsp servers 
+vim.diagnostic.config {
+  virtual_text = {
+    spacing = 2,
+    source = "if_many",
+    prefix = "●",
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = "if_many",
+  },
+}
+
+vim.lsp.config("clangd", {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    vim.lsp.inlay_hint.enable(true)
+    on_attach(client, bufnr)
+  end,
+  on_init = on_init,
+  capabilities = capabilities,
+  root_markers = { ".clangd", "compile_commands.json" },
+  filetypes = { "c", "cpp" },
+  cmd = {
+    "clangd",
+    "--clang-tidy",
+    "--completion-style=detailed",
+    "--background-index",
+    "--offset-encoding=utf-8",
+  },
+  init_options = {
+    clangdFileStatus = true, -- Provides information about activity on clangd’s per-file worker thread
+    usePlaceholders = true,
+    completeUnimported = true,
+    semanticHighlighting = true,
+  },
+})
+
+vim.lsp.enable "clangd"
