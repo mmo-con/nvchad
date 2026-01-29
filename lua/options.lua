3698f8a16ca6588c6322158d3966d7b4b7ccaddf
Markus Moll <markus.moll@hacon.de>
2026-01-29 21:58:24 +0100

    Disable smartindent

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,7 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
-vim.opt.smartindent = true
+vim.opt.smartindent = false
 vim.opt.shell = 'zsh'
 vim.opt.relativenumber = true
 
